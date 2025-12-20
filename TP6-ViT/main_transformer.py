2bc7d867f8441e47797a855fe7ee8de52847a5aa
msaunier <mathis.saunier@insa-rouen.fr>
2025-12-21 00:56:27 +0100

    Repassage au dataset facile

--- TP6-ViT/main_transformer.py
+++ TP6-ViT/main_transformer.py
@@ -187,10 +187,10 @@ if __name__ == '__main__':
         'START_TOKEN':12, # 10 = Horizontal 11 = Vertical
         'END_TOKEN':13,
     }
-    data_file = 'MNIST_5digitsDifficile.pkl' 
+    # data_file = 'MNIST_5digitsDifficile.pkl' 
     #data_file = 'MNIST_5digits2DHorizontal.pkl'
     #data_file =  MNIST_5digits2DVertical.pkl' 
-    # data_file = 'MNIST_5digits2DHorizontalFacile.pkl'
+    data_file = 'MNIST_5digits2DHorizontalFacile.pkl'
     x_train,x_test,y_train,y_test = Load_MNISTSequences(data_file,config)
     N_train = len(y_train)
 
@@ -200,12 +200,12 @@ if __name__ == '__main__':
     N_max = 60000 # digits
     N_max_seq = int(N_max / l_seq_digits) # 60000 / 5 digits
 
-    N_train = 54000 # digits 54000
+    N_train = 100 # digits 54000
     N_train_seq = int(N_train / l_seq_digits)
     END_TRAIN = N_train_seq # 90% 10 000 digits = 5 X 2000
 
     N_batch = int(N_train_seq / config['batch_size'])
-    N_valid = 6000 #6000
+    N_valid = 60 #6000
     N_valid_seq = int(N_valid / l_seq_digits)
     START_VALID = N_max_seq - N_valid_seq  # 10% for validation 1000 digits = 5 X 200
 
